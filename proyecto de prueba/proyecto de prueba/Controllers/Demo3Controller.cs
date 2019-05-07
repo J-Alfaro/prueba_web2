58b12c529e34d22ad1cbc82e1c1a976cf4b17e5f
-]KyN[-Negrotee <49072814+kynnegrotee@users.noreply.github.com>
2019-05-07 17:38:22 -0500

    V4

---
+++ proyecto de prueba/proyecto de prueba/Controllers/Demo3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto_de_prueba.Controllers
+{
+    public class Demo3Controller : Controller
+    {
+        // GET: Demo3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
